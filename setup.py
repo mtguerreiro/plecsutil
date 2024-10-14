4d865dc6e96643c593406724726c1b78ea218a8d
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2024-10-14 17:11:01 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='plecsutil',
+      version='0.1',
+      description='PLECS utility for python',
+      url='https://github.com/mtguerreiro/plecsutil.git',
+      author='Marco Guerreiro',
+      author_email='marcotulio.guerreiro@gmail.com',
+      packages=['plecsutil'],
+      )
