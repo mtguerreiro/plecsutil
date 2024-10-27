bc140ba360f166fc523accaea1bf197985b52861
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2024-10-27 12:06:54 +0100

    Fixing conf.py and requirements for docs generation

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys, os
+
+sys.path.insert(0, os.path.abspath('../'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'plecsutil'
-copyright = '2024, Marco Guerreiro'
+copyright = '2024, plecsutil developers'
 author = 'Marco Guerreiro'
 release = 'v0.1'
 
